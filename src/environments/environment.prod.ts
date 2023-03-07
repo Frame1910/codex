4abc1a70507b638ce070a8f5f7446e2a0b46fece
Darren Meiring <darren.meiring@ibm.com>
2023-03-07 15:12:29 +0800

    added prod keys

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://ruqbzfqkjnzbmcxzkrag.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1cWJ6ZnFram56Ym1jeHprcmFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNzI0MDcsImV4cCI6MTk5Mzc0ODQwN30.eQgncGKXNrTWEq_4hb1KGnhH26PUHiBmqi8DKa_1w9E',
+}
